ad234acf02c8e8009f5e89d45ec8e75966640070
Wade-Philander <wadephilander7@gmail.com>
2021-07-05 14:05:13 +0200

    updated

--- meetups/views.py
+++ meetups/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
+
+from .models import Meetup
+
 # from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    meetups = [
-        {'title': 'A First Meetup', 
-        'location':'New York', 
-        'slug': 'a-first-meetup'},
+ # BELOW IS MOCK DATA 
+    # meetup = [
+    #     {'title': 'A First Meetup', 
+    #     'location':'New York', 
+    #     'slug': 'a-first-meetup'},
 
-        {'title': 'A Second Meetup', 
-        'location':'Paris', 
-        'slug': 'a-second-meetup'}
-    ]
-    return render(request,'index.html',{
-        'show_meetups': True,
+    #     {'title': 'A Second Meetup', 
+    #     'location':'Paris', 
+    #     'slug': 'a-second-meetup'}
+    # ]
+    # return render(request,'meetups/index.html',{
+    #     'show_meetups': True,
+    #     'meetups': meetups
+    # })
+
+def index(request):
+    meetups = Meetup.objects.all()
+
+    return render(request,'meetups/index.html',{
         'meetups': meetups
     })
 
@@ -22,7 +32,7 @@ def meetup_details(request, meetup_slug):
     
     selected_meetups = {'title':'A First Meetup', 'description':'This is our first meetup!'}
     
-    return render(request, 'meetup-detail.html',{
-        'meetup_title': selected_meetups['title'],
-        'meetup_description': selected_meetups['description'],
+    return render(request, 'meetups/meetup-detail.html',{
+        'meetup_title': selected_meetups.title,
+        'meetup_description': selected_meetups.description
     })
